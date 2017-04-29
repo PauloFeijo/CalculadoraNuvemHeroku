f62b94819bcd260c4601dc7ebe40df40b6269d65
Paulo Ricardo Feijo <Paulo Ricardo Feijo>
2017-04-29 10:41:43 -0300

    mensagem para versão

---
+++ src/main/java/br/edu/fcv/rest/MessageRestService.java
@@ -0,0 +1,18 @@
+package br.edu.fcv.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/message")
+public class MessageRestService {
+
+	@GET
+	@Path("/{param}")
+	public Response printMessage(@PathParam("param") String msg) {
+		String result = "Restful example alterado : " + msg;
+		return Response.status(200).entity(result).build();
+	}
+
+}
