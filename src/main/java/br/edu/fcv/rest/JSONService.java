ce03d469a351b26cfd002c397d721f22742f9d32
Paulo Ricardo Feijo <Paulo Ricardo Feijo>
2017-04-29 15:03:39 -0300

    acesso ao banco

--- src/main/java/br/edu/fcv/rest/JSONService.java
+++ src/main/java/br/edu/fcv/rest/JSONService.java
@@ -1,5 +1,8 @@
 package br.edu.fcv.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -7,6 +10,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+import br.edu.fcv.model.ApiResponse;
 import br.edu.fcv.model.Produto;
 
 @Path("/json/product")
@@ -20,6 +24,7 @@ public class JSONService {
 		Produto produto = new Produto();
 		produto.setNome("Moto X - Android");
 		produto.setQuantidade(10);
+
 		return produto;
 
 	}
@@ -32,6 +37,33 @@ public class JSONService {
 		String result = "Product created : " + produto;
 		return Response.status(201).entity(result).build();
 
+	}
+
+	@GET
+	@Path("/listaprodutos")
+	@Produces("application/json")
+	public ApiResponse getListaProdutos() {
+		List<Produto> novaLista = new ArrayList<Produto>();
+
+		Produto Moto = new Produto();
+		Moto.setNome("Moto X - Android");
+		Moto.setQuantidade(10);
+		novaLista.add(Moto);
+
+		Produto Galaxy = new Produto();
+		Galaxy.setNome("Galaxy 7");
+		Galaxy.setQuantidade(15);
+		novaLista.add(Galaxy);
+
+		Produto Iphone = new Produto();
+		Iphone.setNome("Iphone");
+		Iphone.setQuantidade(5);
+		novaLista.add(Iphone);
+
+		ApiResponse apiResponse = new ApiResponse();
+		apiResponse.setProdutos(novaLista);
+
+		return apiResponse;
 	}
 
 }
