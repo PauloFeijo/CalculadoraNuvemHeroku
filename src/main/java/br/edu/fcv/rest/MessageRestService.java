5ddc9400e20117d9f5a82dc0de1209071e869d21
Paulo Ricardo Feijo <Paulo Ricardo Feijo>
2017-04-29 11:21:01 -0300

    criado o soma

--- src/main/java/br/edu/fcv/rest/MessageRestService.java
+++ src/main/java/br/edu/fcv/rest/MessageRestService.java
@@ -5,13 +5,22 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
-@Path("/message")
+@Path("/calculadora")
 public class MessageRestService {
 
 	@GET
 	@Path("/{param}")
 	public Response printMessage(@PathParam("param") String msg) {
 		String result = "Restful example alterado : " + msg;
+		return Response.status(200).entity(result).build();
+	}
+
+	@GET
+	@Path("/soma/{n1}/{n2}")
+	public Response printMessage(@PathParam("n1") Double n1, @PathParam("n2") Double n2) {
+
+		String result = "Resultado da soma : " + (n1 + n2);
+
 		return Response.status(200).entity(result).build();
 	}
 
